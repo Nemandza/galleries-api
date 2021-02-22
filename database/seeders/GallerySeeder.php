bd3d7714b74551bac247cd18139701d2dbf5676a
Nemandza <milicn18@gmail.com>
2021-02-22 20:22:50 +0100

    Created galleries table, migrated, created gallery model, factory and seeder

--- database/seeders/GallerySeeder.php
+++ database/seeders/GallerySeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Gallery;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class GallerySeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([GallerySeeder::class]);
+        Gallery::factory(10)->create();
     }
 }
