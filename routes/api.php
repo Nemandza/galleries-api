429589724507575544f8d95238fad145236e0c15
Nemandza <milicn18@gmail.com>
2021-02-23 21:29:57 +0100

    Used tymon jwt package, implemented authentication with AuthController, RegisterRequest and routes, plus added users in users table

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GalleryController;
@@ -19,4 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('galleries', GalleryController::class);
+Route::resource('galleries', GalleryController::class)->middleware('auth:api');
+
+Route::post('register', [ AuthController::class, 'register' ])->middleware('guest:api');
+Route::post('login', [ AuthController::class, 'login' ])->middleware('guest:api');
+Route::post('logout', [ AuthController::class, 'logout' ])->middleware('auth:api');
